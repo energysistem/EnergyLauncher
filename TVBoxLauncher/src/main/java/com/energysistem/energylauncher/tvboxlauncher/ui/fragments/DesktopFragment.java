3b7a5a8c392d61ff9970ec4556e526479df6aae0
vgt <vginert@gmail.com>
2014-04-15 10:38:34 +0200

    Added vgt branch

--- TVBoxLauncher/src/main/java/com/energysistem/energylauncher/tvboxlauncher/ui/fragments/DesktopFragment.java
+++ TVBoxLauncher/src/main/java/com/energysistem/energylauncher/tvboxlauncher/ui/fragments/DesktopFragment.java
@@ -1,9 +1,7 @@
 package com.energysistem.energylauncher.tvboxlauncher.ui.fragments;
 
 import android.app.Fragment;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,13 +9,8 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.energysistem.energylauncher.tvboxlauncher.R;
-import com.energysistem.energylauncher.tvboxlauncher.modelo.AppInfo;
 import com.energysistem.energylauncher.tvboxlauncher.modelo.ShortcutInfo;
-import com.energysistem.energylauncher.tvboxlauncher.modelo.WebPageInfo;
 import com.energysistem.energylauncher.tvboxlauncher.ui.adapters.ShortcutAdapter;
-import com.energysistem.energylauncher.tvboxlauncher.ui.views.Shortcut;
-
-import java.net.MalformedURLException;
 
 /**
  * Created by vgt on 11/04/2014.
@@ -30,20 +23,11 @@ public class DesktopFragment extends Fragment implements AdapterView.OnItemClick
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_launcher, container, false);
+        View view = inflater.inflate(R.layout.fragment_desktop, container, false);
 
         mAppsGrid = (GridView) view.findViewById(R.id.app_grid);
         gridAdapter = new ShortcutAdapter(getActivity());
         mAppsGrid.setAdapter(gridAdapter);
-
-        for(int i = 0; i < 10; i++) {
-            try {
-                WebPageInfo webPageInfo = new WebPageInfo("http://www.google.es");
-                gridAdapter.addItem(webPageInfo);
-            } catch (MalformedURLException e) {
-                Log.d("DesktopFragment", "Invalid Url");
-            }
-        }
 
         mAppsGrid.setOnItemClickListener(this);
 
--- TVBoxLauncher/src/main/java/com/energysistem/energylauncher/tvboxlauncher/ui/fragments/MenuBookMarkFragment.java
+++ TVBoxLauncher/src/main/java/com/energysistem/energylauncher/tvboxlauncher/ui/fragments/MenuBookMarkFragment.java
@@ -9,7 +9,10 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.energysistem.energylauncher.tvboxlauncher.R;
+import com.energysistem.energylauncher.tvboxlauncher.modelo.WebPageInfo;
 import com.energysistem.energylauncher.tvboxlauncher.ui.LauncherActivity;
+
+import java.net.MalformedURLException;
 
 /**
  * Created by MFC on 14/04/2014.
@@ -19,13 +22,21 @@ public class MenuBookMarkFragment  extends Fragment {
         @Override
         public View onCreateView (LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState){
             View view = inflater.inflate(R.layout.fragment_menu_bookmark, container, false);
-            TextView txtName = (TextView) view.findViewById(R.id.menuBookmarkTxtName);
-            TextView txtUri = (TextView) view.findViewById(R.id.menuBookmarkTxtUri);
+            final TextView txtName = (TextView) view.findViewById(R.id.menuBookmarkTxtName);
+            final TextView txtUri = (TextView) view.findViewById(R.id.menuBookmarkTxtUri);
             //Button btnSave = (Button) view.findViewById(R.id.menuBookmarkBtn);
             Button btnSave = (Button) view.findViewById(R.id.menuBookmarkBtn);
             btnSave.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View v) {
-                    // Perform action on click
+                    WebPageInfo info = null;
+                    try {
+                        info = new WebPageInfo(txtUri.getText().toString());
+                        info.title = txtName.getText().toString();
+                        ((LauncherActivity) getActivity()).addShortcut(info);
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                        //TODO
+                    }
                 }
             });
 
