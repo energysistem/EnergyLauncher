35d9c5e8c9ed23696f0fc406934cf881ef2d7c8f
emg <testeban@gmail.com>
2014-04-17 19:25:11 +0200

    Limpieza ActivityMain

--- TVBoxLauncher/src/main/java/com/energysistem/energylauncher/tvboxlauncher/ui/LauncherActivity.java
+++ TVBoxLauncher/src/main/java/com/energysistem/energylauncher/tvboxlauncher/ui/LauncherActivity.java
@@ -1,5 +1,17 @@
 package com.energysistem.energylauncher.tvboxlauncher.ui;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
 import com.energysistem.energylauncher.tvboxlauncher.LauncherAppState;
 import com.energysistem.energylauncher.tvboxlauncher.R;
 import com.energysistem.energylauncher.tvboxlauncher.modelo.AppInfo;
@@ -8,65 +20,15 @@ import com.energysistem.energylauncher.tvboxlauncher.modelo.ShortcutInfo;
 import com.energysistem.energylauncher.tvboxlauncher.ui.fragments.AppListFragment;
 import com.energysistem.energylauncher.tvboxlauncher.ui.fragments.DesktopFragment;
 import com.energysistem.energylauncher.tvboxlauncher.ui.fragments.MenuListFragmernt;
-import com.energysistem.energylauncher.tvboxlauncher.ui.fragments.SelectedAppsListFragment;
-import com.energysistem.energylauncher.tvboxlauncher.util.SystemUiHider;
-
-import android.app.Activity;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.support.v4.app.ActionBarDrawerToggle;
-import android.support.v4.widget.DrawerLayout;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.KeyEvent;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.ImageButton;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
+
 public class LauncherActivity extends Activity implements AppListFragment.Callbacks {
 
-    /**
-     * Whether or not the system UI should be auto-hidden after
-     * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-     */
-    private static final boolean AUTO_HIDE = true;
-
-    /**
-     * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-     * user interaction before hiding the system UI.
-     */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
-
-    /**
-     * If set, will toggle the system UI visibility upon interaction. Otherwise,
-     * will show the system UI visibility upon interaction.
-     */
-    private static final boolean TOGGLE_ON_CLICK = true;
-
-    /**
-     * The flags to pass to {@link SystemUiHider#getInstance}.
-     */
-    private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
-
-    /**
-     * The instance of the {@link SystemUiHider} for this activity.
-     */
-    private SystemUiHider mSystemUiHider;
     SaveLoadAppsPreferences preferencesListadoApps;
     private AppListFragment appListFragment;
-    private SelectedAppsListFragment selectedAppsListFragment;
     private DesktopFragment desktopFragment;
 
     private DrawerLayout desktopLayout;
@@ -77,6 +39,7 @@ public class LauncherActivity extends Activity implements AppListFragment.Callba
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         desktopLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -99,7 +62,7 @@ public class LauncherActivity extends Activity implements AppListFragment.Callba
 
             public void onDrawerOpened(View drawerView) {
                 try {
-                    ((ViewGroup)drawerView).getChildAt(0).requestFocus();
+                    ((ViewGroup) drawerView).getChildAt(0).requestFocus();
                 } catch (NullPointerException e) {
 
                 }
@@ -127,8 +90,11 @@ public class LauncherActivity extends Activity implements AppListFragment.Callba
                     .add(R.id.left_drawer, new MenuListFragmernt()).commit();
 
         }
+
+
+
     }
- /*
+    /*
     **************************************************************
     Manejar las preferencias
     **************************************************************
@@ -153,6 +119,8 @@ public class LauncherActivity extends Activity implements AppListFragment.Callba
     public void actualizaArrayAppsPreferencias(List<AppInfo> listaApps){
         preferencesListadoApps.ActualizaListaApps(listaApps);
     }
+
+
 
 
     @Override
