cc6af95f772dad2a766543c5141c303f1386ef11
Pablo Gil <pablo.gil@energysistem.com>
2015-01-23 11:16:46 +0100

    cambios en la branch nueva

--- TVBoxLauncher/src/main/java/com/energysistem/energylauncher/tvboxlauncher/database/bookmarkDAO.java
+++ TVBoxLauncher/src/main/java/com/energysistem/energylauncher/tvboxlauncher/database/bookmarkDAO.java
@@ -20,7 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BookmarkDAO {
-
+        //COMMIT PABLO
     // Database fields
     private SQLiteDatabase database;
     private SqliteHelper dbHelper;
